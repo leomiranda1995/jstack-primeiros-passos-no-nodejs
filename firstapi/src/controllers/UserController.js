8453bb2fe679ce82c20eed1815fd6ae77f6dd43e
Leonardo da Silva Miranda <leomiranda1995@gmail.com>
2022-01-27 16:28:22 -0300

    Recebendo parâmetros na URL

--- firstapi/src/controllers/UserController.js
+++ firstapi/src/controllers/UserController.js
@@ -14,5 +14,19 @@ module.exports = {
 
     response.writeHead(200, { 'Content-Type': 'application/json' });
     response.end(JSON.stringify(users));
+  },
+
+  getUserById(request, response) {
+    const { id } = request.params;
+
+    const user = users.find((user) => user.id === Number(id));
+
+    if (user){
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(user));
+    } else {
+      response.writeHead(400, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify({ error: 'User not found' }));
+    }
   }
 }
